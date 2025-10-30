5c8c7bc74945d7d6dd3889f4e293a9b7425dc575
RomaBureacov <RomanBureacov@gmail.com>
2025-10-29 17:50:31 -0700

    fixed up the batch script to help simplify compilation

--- compile.bat
+++ compile.bat
@@ -1,6 +1,10 @@
+@echo off
 REM ELijah made these commands:
 REM javac -d bin src/jminusminus/**.java
 REM java -cp bin jminusminus/Main {{ARGS}}
 
 REM this might be useful:
 REM java -cp lib\j--.jar jminusminus.Main
+
+@echo on
+java -cp lib\j--.jar jminusminus.Main %~1
